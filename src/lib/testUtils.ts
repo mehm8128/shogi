e579fcda76f6fef56089c7226e2f18aee3214bb6
mehm8128 <mehm8128@gmail.com>
2024-04-20 11:26:44 +0900

    型を修正

--- src/lib/testUtils.ts
+++ src/lib/testUtils.ts
@@ -1,20 +1,15 @@
-import { Coordinate } from '@/features/piece/schema'
+import { Coordinate, Piece } from '@/features/piece/schema'
 
 export const compareCoordinates = (a: Coordinate, b: Coordinate) =>
 	Number(`${a.x}${a.y}`) - Number(`${b.x}${b.y}`)
 
 export const nullPiece = { id: '', type: null, own: null, promoted: false }
-export const nullBoard = Array(9).fill(
+export const nullBoard: Piece[][] = Array(9).fill(
 	Array(9)
 		.fill(nullPiece)
 		.map(piece => ({ ...piece, id: 'test' }))
 )
 
-export const arrayWith = <T>(
-	arr: T[][],
-	a: number,
-	b: number,
-	val: NoInfer<T>
-) => {
+export const arrayWith = <T>(arr: T[][], a: number, b: number, val: T) => {
 	return arr.with(a, arr[a].with(b, val))
 }
